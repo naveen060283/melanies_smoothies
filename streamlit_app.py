f54e82af2ab0750e98fb0dbd69f1f291414922ae
naveen060283 <83172220+naveen060283@users.noreply.github.com>
2025-07-20 20:00:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import python packages
+#import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-#import json
+import json
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
