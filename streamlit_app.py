b3c96b801e80b1c92b756da2c3725768a4cba94a
naveen060283 <83172220+naveen060283@users.noreply.github.com>
2025-07-22 10:46:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 #pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
-    'Choose upto 5 ingredients:'
+    'Choose upto 5 ingredients:',
     #, my_dataframe
     max_selections=5
     )
@@ -30,7 +30,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
       
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Information')
